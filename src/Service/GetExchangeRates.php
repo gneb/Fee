ba9632197f8caa51f2ed082e62311807c679fd3f
gneb <gneb@outlook.com>
2024-03-05 11:29:09 +0400

    service to get rates

--- src/Service/GetExchangeRates.php
+++ src/Service/GetExchangeRates.php
@@ -9,6 +9,6 @@ class GetExchangeRates
     public static function execute()
     {
         global $ENV;
-        return Fetch::get("{$ENV['EXCHANGE_RATE_API_URL']}?apikey={$ENV['EXCHANGE_RATE_API_KEY']}");
+        return Fetch::get("{$ENV['EXCHANGE_RATE_API_URL']}/v1/latest?access_key={$ENV['EXCHANGE_RATE_API_KEY']}&base={$ENV['EXCHANGE_RATE_BASE_CURRENCY']}");
     }
 }
